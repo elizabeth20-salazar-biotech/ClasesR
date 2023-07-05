02564c4beeda0719a9959cdf1031d02e9b7e6a89
Elizabeth Salazar <elizasalazarm20@hotmil.com>
2023-07-05 17:21:22 -0500

    no entendi ni vrg :c

--- fdf.R
+++ fdf.R
@@ -1,1 +1,42 @@
 # hola probando
+head(iris,2)
+str(iris)
+summary(iris)
+plot(iris)
+
+library(ggplot2)
+ggplot(iris, aes(x = Species, y = Sepal.Length
+                fill = Species))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Analisis de relacion
+#Identificar si las variables independientes afectan a las variables independientes.
+
+$ y= \beta $
+
+
